aaadebb667ad660cf59fb8f7f492aace07c8424f
grodek <Lukasz.Grodek@wjwgmbh.local>
2019-05-09 15:53:57 +0200

    added a new blog form and fixed a bugs

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,8 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Asset\Package;
 use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 
 
@@ -37,6 +45,7 @@ class BlogController extends AbstractController{
 
 
      }
+
     /**
      * Matches /blog exactly
      * @Route("/blog", name="blog_list")
@@ -58,12 +67,52 @@ class BlogController extends AbstractController{
      }
 
      /**
+      * Matches /saveblog exactly
+      * @Route("/saveblog", name="saveblog")
+      */
+      public function saveBlog(Request $request){
+
+        $blog = new Blog();
+        $blog->setTitle("my new Blog");
+        $blog->setDescription("starts here ...");
+        $blog->setCreatedAt(new \DateTime());
+
+        $form = $this->createFormBuilder($blog)
+            ->add('title',TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('save',SubmitType::class, ['label'=>'Create Blog'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&&$form->isValid()){
+
+          $blog = $form->getData();
+
+          $entityManager = $this->getDoctrine()->getManager();
+
+          $entityManager->persist($blog);
+
+          $entityManager->flush();
+
+          
+            return $this->redirectToRoute('blog_list');
+
+        }
+
+        return $this->render('blog/saveBlog.html.twig', [
+          'controller_name' => "Blog save",
+          'form' => $form->createView()
+        ]); 
+
+      }
+
+     /**
       * Matches /blog/*
       * but not /blog/slug/extra-part
       * @Route("/blog/{slug}", name="blog_show")
       */
-
-      public function show($slug){
+            public function show($slug){
 
         $blog = $this->getDoctrine()
         ->getRepository(Blog::class)
